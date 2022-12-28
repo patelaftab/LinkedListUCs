df7a2214a973bc51dea5de637f0f25d53ed97cab
patelaftab <10patelaftab@gmail.com>
2022-12-28 19:28:11 +0530

    [Aftab] Creating a Simple Linked List

---
+++ LinkedList/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LinkedList
+{
+    class program
+    {
+        public static void Main(String[] args) 
+        {
+            Console.WriteLine("Welcome To Linked List Program ");
+            Console.WriteLine("Choose a option from below\n" +
+                "\nChoose 1): To Creating a linked list ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch(option)
+            {
+                case 1:
+                    Linkedlist linkedList = new Linkedlist();
+                    linkedList.Add(56);
+                    linkedList.Add(30);
+                    linkedList.Add(70);
+                    linkedList.Display();
+                    break;
+                default:
+                    Console.WriteLine("Choose a Valid Option");
+                    break;
+            }
+        }
+    }
+}
